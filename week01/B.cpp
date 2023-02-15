a316d00e3461a7607203956bc79d6676bd7e9756
Dmitriy Nikiforov <dmitriy-nik09@mail.ru>
2023-02-15 13:10:51 +0300

    fourth version

--- week01/B.cpp
+++ week01/B.cpp
@@ -4,18 +4,21 @@
 using namespace std; 
 
 int main() {
-    int a = 0, b = 0, c = 0;
+    float a = 0, b = 0, c = 0;
     float x_1 = 0, x_2 = 0, D = 0;
     
     cin >> a >> b >> c;
 
     D = sqrt(b*b - 4*a*c);
-    if (D > 0) {
+    if (D > 0 && a != 0) {
         x_1 = (-b + D)/(2*a);
         x_2 = (-b - D)/(2*a);
         cout << x_1 << ' ' << x_2 << endl;
-    } else if (D == 0) {
+    } else if (D == 0 && a != 0) {
         x_1 = (-b)/(2*a);
+        cout << x_1 << endl;
+    } else if (a == 0) {
+        x_1 = (-c)/(b);
         cout << x_1 << endl;
     } else {
         cout << '\n' << endl;
