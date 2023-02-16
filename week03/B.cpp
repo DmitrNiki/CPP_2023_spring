2d1b417e0773619f631156a1a76c1a0557dbed2d
Dmitriy Nikiforov <dmitriy-nik09@mail.ru>
2023-02-16 17:48:53 +0300

    demo version

---
+++ week03/B.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#ifndef N
+#define N 10  // actual size of the array
+#endif
+
+using namespace std;
+
+void swap(int &lha, int &rha){
+    int tmp = lha;
+    lha = rha;
+    rha = tmp;
+}
+
+void arr_input(int arr[N]){
+    int x = 0;
+    for (int i = 0; i < N; i++){
+        cin >> x;
+        arr[i] = x;
+    }
+}
+
+void arr_ringRmove(int arr[N]){
+    int tmp = 0;
+    for (int i = 0; i <= N - 1 ; i++){
+        swap(arr[N - 2 - i], arr[N - 1 - i]);   
+    }
+}
+
+ void arr_output(int arr[N]){
+    for (int i = 0; i < N; i++){
+        cout << arr[i] << " "; 
+    }
+    cout << endl;
+ }
+
+int main() {
+    int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  // array initialization
+    arr_input(a);
+    arr_ringRmove(a);
+    arr_output(a);
+    return 0;
+}
---
+++ week03/C.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#ifndef N
+#define N 10  // actual size of the array
+#endif
+
+using namespace std;
+
+void swap(int &lha, int &rha){
+    int tmp = lha;
+    lha = rha;
+    rha = tmp;
+}
+
+void arr_input(int arr[N]){
+    for (int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+}
+
+void arr_ringRmove(int arr[N]){
+    int tmp = 0;
+    for (int i = 0; i <= N - 1 ; i++){
+        swap(arr[N - 2 - i], arr[N - 1 - i]);   
+    }
+}
+
+ void arr_output(int arr[N]){
+    for (int i = 0; i < N; i++){
+        cout << arr[i] << " "; 
+    }
+    cout << endl;
+ }
+
+int main() {
+    int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  // array initialization
+    int k = 0;
+    arr_input(a);
+    cin >> k;
+    for (int i = 1; i <= k; i++){
+        arr_ringRmove(a);
+    }
+    arr_output(a);
+    return 0;
+}
