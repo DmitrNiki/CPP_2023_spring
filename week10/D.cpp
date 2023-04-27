d4856ce9f738a8ab87d354e7816a9567d4d16332
Dmitriy Nikiforov <dmitriy-nik09@mail.ru>
2023-04-27 11:42:23 +0300

    recurent grasshoper

--- week10/D.cpp
+++ week10/D.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
-int grasshopper(int n)
+long long int grasshopper(int n){
+    long long int jump[3] = {1, 1, 2};
+
+    if (n <= 2) return jump[n];
+
+    for (int i = 3; i <= n; i++)
+        jump[i % 3] = jump[i%3] + jump[(i + 1)%3] + jump[(i + 2)%3];
+
+    return jump[n % 3];
+}
+
 int main(){
     int n;
     cin >> n;
 
-    if (n >= 0 && n <= 40) cout << grasshopper(n) << endl;
+    if (n >= 1 && n <= 60) cout << grasshopper(n) << endl;
     return 0;
 }
